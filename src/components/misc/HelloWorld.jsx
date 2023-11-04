ec17fb94432668e830fe478e69b36fff1f44f78b
Tantatorn <timefear007@gmail.com>
2023-11-04 13:33:35 +0700

    ✨ add cool looking hello world text

--- src/components/misc/HelloWorld.jsx
+++ src/components/misc/HelloWorld.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import Typed from "typed.js";
 
-const HelloWorld = () => {
-	return <h1>Hello Moto</h1>;
-};
+const helloWorlds = [
+	'console.log("Hello World")',
+	'fmt.Println("Hello World")',
+	'print("Hello World")',
+	'System.out.println("Hello World")',
+];
 
-export default HelloWorld;
+export default function HelloWorld() {
+	const el = useRef(null);
+
+	useEffect(() => {
+		const typed = new Typed(el.current, {
+			strings: helloWorlds,
+			typeSpeed: 50,
+			backSpeed: 50,
+			loop: true,
+		});
+
+		return () => {
+			typed.destroy();
+		};
+	}, []);
+
+	return (
+		<h1 className="mb-6 text-xl text-accent">
+			<span ref={el} />
+		</h1>
+	);
+}
